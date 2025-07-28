3c32b984f8e852e8cb0ffa19736a47fde1afaedb
chirag <rajachirag0@gmail.com>
2025-07-28 13:56:12 +0530

    test comit

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -8,7 +8,6 @@ export const HISTORIC_PREDICTION = "historicprediction";
 
 export const FEATURES = "features";
 
-//download Files
 export const DOWNLOAD = "download";
 export const HISTORIC_DOWNLOAD = "historicdownload";
 
